04ba6fb496b39143ee6fb633f283138d2e5d2cb9
Kumar manak <manakkash20@gmail.com>
2025-06-06 22:34:54 +0530

    hi

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
 from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+db=SQLAlchemy(app)
+class todo(db.Model):
+    sno=db.Column(db.Integer,primary_key=True)
+
+    title=db.Column(db.String(200),nullable=False)
+    desc=db.Column(db.String(400),nullable=False)
+    
+    def __repr__(self) -> str:
+        return f"{self.sno}-{self.title}"
+
 
 @app.route("/")
 def hello_world():
+
     return render_template("index.html")
 if __name__=="__main__":
     app.run(debug=True)
